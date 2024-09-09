34f49e2e7a710ed20eff21e37a4f5376208fbaad
rifatfarabi <rifatfarabi08@gmail.com>
2024-09-09 18:58:07 +0600

    update added

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -31,16 +31,23 @@
                             </tr>
                         </thead>
                         <tbody>
-                            @foreach ($products as $key=>$product)
-                            <tr>
-                                <th scope="row">{{ $key+1 }}</th>
-                                <td>{{ $product->name }}</td>
-                                <td>{{ $product->price }}</td>
-                                <td>
-                                    <a href="" class="btn btn-success"><i class="las la-edit"></i></a>
-                                    <a href="" class="btn btn-danger"><i class="las la-trash"></i></a>
-                                </td>
-                            </tr>
+                            @foreach ($products as $key => $product)
+                                <tr>
+                                    <th scope="row">{{ $key + 1 }}</th>
+                                    <td>{{ $product->name }}</td>
+                                    <td>{{ $product->price }}</td>
+                                    <td>
+                                        <a href="" class="btn btn-success update_product_form"
+                                        data-bs-toggle="modal" data-bs-target="#updateModal"
+                                        data-id = "{{ $product->id }}"
+                                        data-name = "{{ $product->name }}"
+                                        data-price = "{{ $product->price }}"
+                                        >
+                                        <i class="las la-edit"></i>
+                                    </a>
+                                        <a href="" class="btn btn-danger"><i class="las la-trash"></i></a>
+                                    </td>
+                                </tr>
                             @endforeach
                         </tbody>
                     </table>
@@ -51,7 +58,8 @@
     </div>
 
     @include('products_add')
- @include('products_js')
+    @include('product_update')
+    @include('products_js')
 
 
 </body>
--- resources/views/products_js.blade.php
+++ resources/views/products_js.blade.php
@@ -42,5 +42,15 @@ $(document).ready(function() {
             }
         });
     });
+
+    $(document).on('click','.update_product_form', function(){
+        let id = $(this).data('id');
+        let name = $(this).data('name');
+        let price = $(this).data('price');
+
+        $('#up_id').val(id);
+        $('#up_name').val(name);
+        $('#up_price').val(price);
+    })
 });
 </script>
